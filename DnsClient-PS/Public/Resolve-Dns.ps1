654490585de7d0f458689b71f8f111ce148ba537
Ryan Bolger <z97y9drumh@snkmail.com>
2020-05-19 00:29:23 -0700

    fixed PTR queries with explicit in-addr.arpa names

--- DnsClient-PS/Public/Resolve-Dns.ps1
+++ DnsClient-PS/Public/Resolve-Dns.ps1
@@ -15,14 +15,23 @@ function Resolve-Dns {
     $client = Get-LookupClient
     $nsList = Get-NameServerList $NameServer
 
-    # Reverse Lookup
-    if ([QueryType]::PTR -eq $QueryType) {
+    # Check for IP based PTR
+    if ($QueryType -eq [QueryType]::PTR -and
+        $Query -notmatch '\.in-addr\.arpa(?:\.)?')
+    {
+        # We're going to assume anyone asking for a PTR
+        # where the query doesn't end with .in-addr.arpa
+        # has given us an IP that we can use with
+        # QueryServerReverse.
 
         $client.QueryServerReverse($nsList, $Query)
 
     } else {
 
-        $client.QueryServer($nsList, $Query, $QueryType, $QueryClass)
+        # Everything else should be a standard query question
+        $question = [DnsQuestion]::new($Query, $QueryType, $QueryClass)
+
+        $client.QueryServer($nsList, $question)
 
     }
 
